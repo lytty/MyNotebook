4219d2655ac5377142c471edb3c3351da410161d
Haibin Xu <haibin8936@sina.com>
2018-08-21 13:41:19 +0800

    update mysite

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -8,10 +8,11 @@ from .models import Question
 def index(request):
 	latest_question_list = Question.objects.order_by('-pub_date')[:5]
 	template = loader.get_template('polls/index.html')
-    context = {
+	context = {
         'latest_question_list': latest_question_list,
-    }
-    return HttpResponse(template.render(context, request))
+	}
+
+	return HttpResponse(template.render(context, request))
 
     # output = ', '.join([q.question_text for q in latest_question_list])
     # return HttpResponse(output)
